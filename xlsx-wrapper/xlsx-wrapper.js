5e835495d828cf7c280bf3a552746795e904c858
Meska <meskatech@gmail.com>
2022-12-03 16:37:11 +0100

    Testing

--- xlsx-wrapper/xlsx-wrapper.js
+++ xlsx-wrapper/xlsx-wrapper.js
@@ -1,14 +1,14 @@
-import xlsx from 'node-xlsx';
+var xlsx = require("jnode-xlsx");
 
 module.exports = function (RED) {
     function XlsxWrapper(config) {
         RED.nodes.createNode(this, config);
-        let  node = this;
-        node.on('input', function (msg) {
+        let node = this;
+        node.on("input", function (msg) {
             msg.payload = xlsx.build(msg.payload);
             node.send(msg);
         });
     }
 
     RED.nodes.registerType("xlsx-wrapper", XlsxWrapper);
-}
+};
