af26a0e89d72b50d894837263e81eeed5f784397
meska <meskatech@gmail.com>
2022-12-03 12:23:48 +0100

    first version

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-red-condtrib-xlsx-wrapper
+
+This node converts payload to an xls file, for the format specification see node-xlsx package
---
+++ xlsx-wrapper/xlsx-wrapper.js
@@ -0,0 +1,14 @@
+import xlsx from 'node-xlsx';
+
+module.exports = function (RED) {
+    function XlsxWrapper(config) {
+        RED.nodes.createNode(this, config);
+        let  node = this;
+        node.on('input', function (msg) {
+            msg.payload = xlsx.build(msg.payload);
+            node.send(msg);
+        });
+    }
+
+    RED.nodes.registerType("xlsx-wrapper", XlsxWrapper);
+}
